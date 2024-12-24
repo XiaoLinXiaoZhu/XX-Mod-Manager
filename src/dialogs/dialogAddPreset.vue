028675fee4ae76791674ab6dc1d729f650cefbee
XLXZ <helloXLXZ233@gmail.com>
2024-12-24 10:50:50 +0800

    优化添加预设对话框样式，调整组件引用和样式规则

--- src/dialogs/dialogAddPreset.vue
+++ src/dialogs/dialogAddPreset.vue
@@ -1,21 +1,25 @@
 <template>
-    <s-dialog id="add-preset-dialog">
-      <div slot="headline" class="font-hongmeng">
-        <p data-translate-key="ask-preset-name">请输入预设名称</p>
-        <div class="OO-setting-bar">
-            <h3>预设名称</h3>
-            <s-text-field v-model="presetName" placeholder="请输入预设名称" id="preset-name">
+    <s-dialog id="add-preset-dialog" ref="componentRef">
+        <div slot="headline" class="font-hongmeng">
+            <p data-translate-key="ask-preset-name">请输入预设名称</p>
+            <div class="OO-setting-bar">
+                <h3>预设名称</h3>
+                <s-text-field v-model="presetName" placeholder="请输入预设名称" id="preset-name">
 
-            </s-text-field>
+                </s-text-field>
+            </div>
+
+            <div style="">
+            </div>
         </div>
-      </div>
-      <s-button slot="action" type="text" id="dialog-cancel" data-translate-key="cancel">取消</s-button>
-      <s-button slot="action" type="text" id="preset-add-confirm" data-translate-key="confirm" @click="handleAddPreset">确认</s-button>
+        <s-button slot="action" type="text" id="dialog-cancel" data-translate-key="cancel">取消</s-button>
+        <s-button slot="action" type="text" id="preset-add-confirm" data-translate-key="confirm"
+            @click="handleAddPreset">确认</s-button>
     </s-dialog>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref, useTemplateRef } from 'vue'
 import IManager from '../../electron/IManager';
 const { ipcRenderer } = require('electron');
 const iManager = new IManager();
@@ -49,11 +53,51 @@ function handleAddPreset() {
     //清空输入框
     presetName.value = '';
 }
+
+
+const componentRef = useTemplateRef("componentRef");
+onMounted(() => {
+    const editModInfoDialogStyle = document.createElement('style');
+    editModInfoDialogStyle.innerHTML = `
+    .wrapper.show .scrim {
+      background:
+        radial-gradient(black 15%, transparent 16%) 0 0,
+        radial-gradient(black 15%, transparent 16%) 8px 8px,
+        radial-gradient(rgba(255, 255, 255, .1) 15%, transparent 20%) 0 1px,
+        radial-gradient(rgba(255, 255, 255, .1) 15%, transparent 20%) 8px 9px !important;
+    background-color: #282828 !important;
+    background-size: 16px 16px !important;
+    opacity: 0.9 !important;
+filter: blur(1px) !important;
+backdrop-filter: blur(0px) !important;
+
+    }
+    .container {
+      width: 100% !important;
+      max-width: 100% !important;
+      border-radius: 0 !important;
+      height: fit-content !important;
+      min-height: 500px !important;
+      overflow: hidden !important;
+      flex:1;
+      padding-bottom: 30px;
+    }
+    .action {
+      display: none !important;
+    }
+    s-scroll-view{
+      display: none;
+    }    
+        `;
+    // editModInfoDialog.shadowRoot.appendChild(editModInfoDialogStyle);
+    console.log(componentRef.value);
+    componentRef.value.shadowRoot.appendChild(editModInfoDialogStyle);
+
+});
 </script>
 
 <style scoped>
-p{
+p {
     margin: 10px;
 }
-
 </style>
