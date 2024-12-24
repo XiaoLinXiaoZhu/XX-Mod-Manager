f5245ad01291caac3807a6c9dc5e47103e826fa1
XLXZ <helloXLXZ233@gmail.com>
2024-12-24 12:15:30 +0800

    优化对话框和样式，调整背景色、阴影和按钮样式

--- src/dialogs/dialogAddPreset.vue
+++ src/dialogs/dialogAddPreset.vue
@@ -12,8 +12,8 @@
             <div style="">
             </div>
         </div>
-        <s-button slot="action" type="text" id="dialog-cancel" data-translate-key="cancel">取消</s-button>
-        <s-button slot="action" type="text" id="preset-add-confirm" data-translate-key="confirm"
+        <s-button slot="action" type="text" id="dialog-cancel" class="OO-button font-hongmeng">取消</s-button>
+        <s-button slot="action" type="text" id="preset-add-confirm" class="OO-button font-hongmeng"
             @click="handleAddPreset">确认</s-button>
     </s-dialog>
 </template>
@@ -60,30 +60,62 @@ onMounted(() => {
     const editModInfoDialogStyle = document.createElement('style');
     editModInfoDialogStyle.innerHTML = `
     .wrapper.show .scrim {
-      background:
-        radial-gradient(black 15%, transparent 16%) 0 0,
-        radial-gradient(black 15%, transparent 16%) 8px 8px,
-        radial-gradient(rgba(255, 255, 255, .1) 15%, transparent 20%) 0 1px,
-        radial-gradient(rgba(255, 255, 255, .1) 15%, transparent 20%) 8px 9px !important;
-    background-color: #282828 !important;
-    background-size: 16px 16px !important;
-    opacity: 0.9 !important;
-filter: blur(1px) !important;
-backdrop-filter: blur(0px) !important;
+        opacity: 1 !important;
+        filter: blur(0px) !important;
+        backdrop-filter: blur(2px) !important;
+    }
+    .wrapper.show .scrim::after {
+      content: "" !important;
+      position: absolute !important;
+      top: 0 !important;
+      left: 0 !important;
+      width: 100% !important;
+      height: 100% !important;
 
+      background-color: #00000000 !important;
+      background-image: repeating-linear-gradient(45deg, transparent, transparent 5px, rgba(90,90,90,.3) 5px, rgba(90,90,90,.3) 10px) !important;
     }
     .container {
       width: 100% !important;
       max-width: 100% !important;
-      border-radius: 0 !important;
+
       height: fit-content !important;
-      min-height: 500px !important;
-      overflow: hidden !important;
+      min-height: 50px !important;
+
+      border-radius: 0 !important;
+      
+      overflow: visible !important;
       flex:1;
-      padding-bottom: 30px;
+      padding-bottom: 50px;
+
+      align-items: center !important;
     }
+    
+    .container::before {
+      content: "" !important;
+      position: absolute !important;
+      top: 0 !important;
+      left: -5% !important;
+      width: 110% !important;
+      height: 100% !important;
+        opacity: 0.4 !important;
+      background-color: var(--s-color-surface-container-high) !important;
+      box-shadow:inset 0 0 0px 6px var(--s-color-outline-variant), inset 1px 1px 0px 9px rgba(0, 0, 0, 0.2) !important;
+
+        background-color:#001;
+        background-image: radial-gradient(black 15%, transparent 16%),
+        radial-gradient(black 15%, transparent 16%);
+        background-size: 10px 10px;
+        background-position: 0 0, 5px 5px;
+
+    }
+
     .action {
-      display: none !important;
+      position: absolute !important;
+      bottom: -30px !important;
+      margin: 0 100px !important;
+      justify-content: center !important;
+      width: 100% !important;
     }
     s-scroll-view{
       display: none;
@@ -99,5 +131,21 @@ backdrop-filter: blur(0px) !important;
 <style scoped>
 p {
     margin: 10px;
+}
+
+div[slot="headline"] {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    max-width: 500px;
+}
+
+s-button[slot="action"] {
+    margin-left: 20px;
+    margin-right: 20px;
+    color: var(--s-color-on-surface);
 }
 </style>
